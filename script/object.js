0cfddba0d713e0ae1cfa06b26a0917811be01a5c
GiordanoCasale <giordanocasale8@gmail.com>
2025-07-16 12:32:26 +0200

    fixed some problem

--- script/object.js
+++ script/object.js
@@ -1,12 +1,12 @@
 //Snack 1
 
-//const hamburger = { name: "Cheese Burger", weight: 250 };
-//const secondBurger = hamburger;
-//secondBurger.name = 'Double Cheese Burger';
-//secondBurger.weight = 500;
+const hamburger = { name: "Cheese Burger", weight: 250 };
+const secondBurger = hamburger;
+secondBurger.name = 'Double Cheese Burger';
+secondBurger.weight = 500;
 
-//console.log(hamburger.name); // Double Cheese Burger
-//console.log(secondBurger.name); // Double Cheese Burger
+console.log(hamburger.name); // Double Cheese Burger
+console.log(secondBurger.name); // Double Cheese Burger
 
 //è stato creato un solo oggetto in memoria
 
